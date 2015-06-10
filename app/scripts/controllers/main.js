06460ca51f0962c3d0bde78242981aa1376af1c0
dagrende <dag.rende@find-out.se>
2015-06-10 07:49:44 +0200

    cleaned up code, renamed data files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,8 +14,7 @@ angular.module('archbrowserApp')
     $scope.flats = [];
     $scope.hits = 0;
 
-    $http.get('/chunk0.json').
-    //$http.get('/sold-soder-150607.json').
+    $http.get('/flats500.json').
       success(function(flats, status, headers, config) {
         $scope.flats = flats;
         var roomsSetValues = d3.set(flats.sold.reduce(function(ack, flat) {if (flat.rooms) {ack.push(flat.rooms)}; return ack;}, [])).values();
@@ -54,11 +53,11 @@ angular.module('archbrowserApp')
     // create or update flat dot map
     function visualize(flats) {
       if (flats) {
-        d3.select(".visualizer")
+        d3.select("#visualizer")
             .datum(flats)
           .call(flatDotViewer()
-            .width($('.visualizer').innerWidth())
-            .height($('.visualizer').innerHeight())
+            .width($('#visualizer').innerWidth())
+            .height($('#visualizer').innerHeight())
             .x(function(d) { return +d.location.position.longitude; })
             .y(function(d) { return +d.location.position.latitude; })
             .key(function(d) { return +d.booliId; })
@@ -94,13 +93,13 @@ angular.module('archbrowserApp')
 
     // initialize UI widgets
     function initUi() {
-      // set height of main view to window height, and keep it so when window resize
-      function setPaperSize() {
+      // set height of main view to window height, and adjust on window resize
+      function setMainViewHeight() {
         $('.win').outerHeight($(window).innerHeight());
         visualize($scope.hits);
       }
-      setPaperSize();
-      $(window).bind('resize', setPaperSize);
+      setMainViewHeight();
+      $(window).bind('resize', setMainViewHeight);
 
       // set adjustable width
       $('.eresizable').resizable({
@@ -112,7 +111,7 @@ angular.module('archbrowserApp')
         handles: 's'
       });
 
-      // $('.tree div').jstree();
+      // make list handle mouse click selection
       $('.selectable').selectable({
         filter:'tbody tr',
         selected: selectListItem,
